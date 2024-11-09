9d8db32ccf9bce75e1861e1a8a276c835c890c45
Trace <65991410+munjuin@users.noreply.github.com>
2024-11-10 00:01:56 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1216 ms, Memory: 215776 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number).slice(1)
+
+const solution = arr => {
+  return arr.sort((a,b) => a-b).join("\n")
+}
+
+console.log(solution(input))
