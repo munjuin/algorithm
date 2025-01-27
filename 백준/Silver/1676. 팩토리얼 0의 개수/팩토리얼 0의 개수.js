0229e1ea5c6a75bde49f678c3c365e4b3751c855
Trace <65991410+munjuin@users.noreply.github.com>
2025-01-27 11:54:12 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 100 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString()
+inputs = Number(input)
+
+function Factorial(num){
+  let answer = 0;
+  
+  while (num >= 5) {
+    answer += parseInt(num / 5);
+    num /= 5;
+  }
+  
+  return answer;
+}
+
+console.log(Factorial(inputs));
