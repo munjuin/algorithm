0dfb30f1b97d9ba8e1b1df319f8e70db1455343f
Trace <65991410+munjuin@users.noreply.github.com>
2025-02-06 09:01:19 +0900

    [Silver IV] Title: 로프, Time: 236 ms, Memory: 26816 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.js
@@ -0,0 +1,9 @@
+let [n, ...ropes] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+ropes = ropes.map(i => +i);
+ropes.sort((a, b) => a-b);
+let min = ropes[0]*n;
+for (let i=1; i<n; i++) {
+    weight = ropes[i]*(n-i);
+    if (min < weight) min = weight;
+}
+console.log(min);
