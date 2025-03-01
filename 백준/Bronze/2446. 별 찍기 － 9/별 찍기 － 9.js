2e4764ef2c3c798a2feb1e6ea1e485c2df32bd26
Trace <65991410+munjuin@users.noreply.github.com>
2025-03-02 00:35:17 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 96 ms, Memory: 9464 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim()
+
+const solution = (input) => {
+  let result = []
+  for(let i = input; i>=1; i--){
+    let side = " ".repeat(input-i)
+    let middle = '*'.repeat(i*2-1)
+    result.push( side + middle )
+  }
+  for(let i = 2; i<=input; i++){
+    let side = " ".repeat(input-i)
+    let middle = '*'.repeat(i*2-1)
+    result.push( side + middle )
+  }
+  
+  return result.join('\n')
+}
+
+console.log(solution(input))
