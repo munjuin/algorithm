cd620d82b04edbe278948408ef23c584b2c71c48
Trace <65991410+munjuin@users.noreply.github.com>
2025-03-21 23:02:06 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 1072 ms, Memory: 16524 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -0,0 +1,34 @@
+const sol = (input) => {
+  const [N, K] = input.split(" ").map(Number);
+  const visit = Array.from({ length: 100100 }, () => 0);
+
+  function bfs(N) {
+    const queue = [];
+    queue.push([N, 0]);
+    visit[N] = 1;
+    while (queue.length) {
+      const [cur, time] = queue.shift();
+      if (cur === K) return time;
+      for (next of [cur * 2, cur - 1, cur + 1]) {
+        if (!visit[next] && next >= 0 && next <= 100000) {
+          visit[next] = 1;
+          if (next == cur * 2) {
+            queue.unshift([next, time]); // 2X로 이동할 때는 시간을 증가시키지 않고, 우선순위를 반영하여 큐의 맨 앞에 넣어준다.
+          } else {
+            queue.push([next, time + 1]); // X-1, X+1로 이동할 때는 시간을 증가시키고, 큐에 순서대로 넣어준다.
+          }
+        }
+      }
+    }
+  }
+  return bfs(N);
+};
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", (line) => {
+    console.log(sol(line));
+  })
+  .on("close", () => {
+    process.exit();
+  });
