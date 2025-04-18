3acfb8523f65ab62563903dec4edf5f30184c4df
Trace <65991410+munjuin@users.noreply.github.com>
2025-04-19 00:07:31 +0900

    [Gold V] Title: LCS, Time: 204 ms, Memory: 24752 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.js
@@ -0,0 +1,24 @@
+function lcsLength(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
+
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const str1 = input[0];
+const str2 = input[1];
+
+console.log(lcsLength(str1, str2));
