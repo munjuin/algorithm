2c836425f36e115bdfff3a0fe96236a8c4e3ef7a
Trace <65991410+munjuin@users.noreply.github.com>
2025-05-01 17:56:33 +0900

    [Gold III] Title: 줄 세우기, Time: 392 ms, Memory: 40652 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.js
@@ -0,0 +1,44 @@
+const input = [];
+
+const strToNumArr = (str) => str.split(' ').map(Number);
+
+require('readline')
+  .createInterface(process.stdin, process.stdout)
+  .on('line', function (line) {
+    input.push(line.trim());
+  })
+  .on('close', function () {
+    const [N, M] = strToNumArr(input.shift());
+    const graph = [];
+    for (let i = 0; i <= N; i++) {
+      graph.push([]);
+    }
+    const inDegrees = Array(N + 1).fill(0);
+
+    input.forEach((str) => {
+      const [prev, next] = strToNumArr(str);
+      graph[prev].push(next);
+      inDegrees[next] += 1;
+    });
+
+    const queue = [];
+    for (let n = 1; n <= N; n++) {
+      if (!inDegrees[n]) {
+        queue.push(n);
+      }
+    }
+
+    const result = [];
+    while (queue.length) {
+      const n = queue.pop();
+      result.push(n);
+      graph[n].forEach((v) => {
+        inDegrees[v] -= 1;
+        if (!inDegrees[v]) {
+          queue.push(v);
+        }
+      });
+    }
+
+    console.log(result.join(' '));
+  });
