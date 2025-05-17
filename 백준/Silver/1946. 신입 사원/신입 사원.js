78c22cb66f661c7744d466becc4d5538082faa3b
Trace <65991410+munjuin@users.noreply.github.com>
2025-05-17 11:01:05 +0900

    [Silver I] Title: 신입 사원, Time: 3212 ms, Memory: 428100 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.js
@@ -0,0 +1,23 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const T = Number(input[0]);
+let A = 1;
+for (let i = 0; i < T; i++) {
+  let N = Number(input[A]);
+  list = [];
+  for (let j = 1; j < N + 1; j++) {
+    const [a, b] = input[A + j].split(" ").map(Number);
+    list.push([a, b]);
+  }
+  list.sort((a, b) => a[0] - b[0]);
+  let S = list[0][1];
+  let cnt = 1;
+  for (let i = 1; i < N; i++) {
+    if (S > list[i][1]) {
+      cnt += 1;
+      S = list[i][1];
+    }
+  }
+  console.log(cnt);
+  A += N + 1;
+}
