9928690925dd0831a229dd5c9a90dd4afacce16f
Trace <65991410+munjuin@users.noreply.github.com>
2025-06-23 23:16:15 +0900

    [Bronze V] Title: 긴자리 계산, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const A = BigInt(input[0]);
+const B = BigInt(input[1]);
+
+const sum = A + B;
+const difference = A - B;
+const product = A * B;
+
+console.log(sum.toString());
+console.log(difference.toString());
+console.log(product.toString());
