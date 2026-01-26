62768f1ecc5211663ed61e463c76edbfba237ee3
Mun Juin <65991410+munjuin@users.noreply.github.com>
2026-01-26 11:57:04 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 46.33 ms, Memory: 31.9 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -1,23 +1,11 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();// "aBcDeFg"
+const result = input.split('').map((alphabet)=>{
+    if(alphabet === alphabet.toUpperCase()){
+        return alphabet.toLowerCase();
+    } else {
+        return alphabet.toUpperCase();
+    }
+}).join("");
 
-let input = [];
-
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
-    
-let arr = [];
-for (let i = 0; i < str.length; i++){
-  if(str[i] === str[i].toUpperCase()){
-    arr.push(str[i].toLowerCase());
-  }else {
-    arr.push(str[i].toUpperCase());
-  }
-}
-console.log(arr.join(''));
-});
+console.log(result);
