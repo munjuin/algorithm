5c552352ee0e5a1791679942d4650dc1810c1204
Mun Juin <65991410+munjuin@users.noreply.github.com>
2026-01-26 12:05:32 +0900

    [level 0] Title: 특수문자 출력하기, Time: 38.94 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.js
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.js
@@ -1,9 +1,1 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.on('close', function () {
-    console.log(`!@#$%^&*(\\'"<>?:;`);
-});
+console.log(`!@#$%^&*(\\'"<>?:;`);
