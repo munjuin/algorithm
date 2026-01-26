277981bc4146822f1a66a0d8d8ffcd998a303497
Mun Juin <65991410+munjuin@users.noreply.github.com>
2026-01-26 11:20:50 +0900

    [level 0] Title: a와 b 출력하기, Time: 57.61 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -1,14 +1,21 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-let input = [];
+// let input = [];
 
-rl.on('line', (line)=>{
-    input = line.split(' ');
-    rl.close();
-}).on('close', ()=>{
-    console.log(`a = ${input[0]} \nb = ${input[1]}`)
-})
+// rl.on('line', (line)=>{
+//     input = line.split(' ');
+//     rl.close();
+// }).on('close', ()=>{
+//     console.log(`a = ${input[0]} \nb = ${input[1]}`)
+// });
+
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();// "4 5"
+const inputSplit = input.split(' ');// ["4", "5"]
+const [a, b] = inputSplit.map(Number);// [4, 5]
+console.log(`a = ${a}\nb = ${b}`)
+
