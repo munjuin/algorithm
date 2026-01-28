defdd20007639d0d77168561e53ce359b5a31b3d
Mun Juin <65991410+munjuin@users.noreply.github.com>
2026-01-28 11:06:30 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 43.92 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/181946. 문자열 붙여서 출력하기/README.md
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 붙여서 출력하기 - 181946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181946?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181946) 
 
 ### 성능 요약
 
-메모리: 32.3 MB, 시간: 50.93 ms
+메모리: 31.8 MB, 시간: 43.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 22일 23:42:00
+2026년 01월 28일 11:06:27
 
 ### 문제 설명
 
--- 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -1,15 +1,4 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    str1 = input[0];
-    str2 = input[1];
-    console.log(str1+str2)
-});
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+const [str1, str2] = input.split(' ')
+console.log(str1+str2)
