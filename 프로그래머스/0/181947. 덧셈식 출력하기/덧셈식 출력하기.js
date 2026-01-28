76e0c5be464bb5c205762ea1dc696e73bdcbb59a
Mun Juin <65991410+munjuin@users.noreply.github.com>
2026-01-28 11:00:54 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 42.09 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();// "4 5"
 const splitString = input.split(' ');// ["4", "5"]
-const numbering = splitString.map((item)=>Number(item))// [4, 5];
+const [a, b] = splitString.map((item)=>Number(item))// [4, 5];
 
-console.log(`${numbering[0]} + ${numbering[1]} = ${numbering[0] + numbering[1]}`)// 4 + 5 = 9
+console.log(`${a} + ${b} = ${a + b}`)// 4 + 5 = 9
