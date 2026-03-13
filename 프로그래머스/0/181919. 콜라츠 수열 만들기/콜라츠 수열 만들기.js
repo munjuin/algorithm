4280d989a5f115fbf99bdeb7fc7bfa0fdb914364
Mun Juin <65991410+munjuin@users.noreply.github.com>
2026-03-13 12:27:57 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.14 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    const result = [n];
+    while (n !== 1){
+        if(n % 2 ===0){
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        result.push(n)
+    }
+    return result;
+}
