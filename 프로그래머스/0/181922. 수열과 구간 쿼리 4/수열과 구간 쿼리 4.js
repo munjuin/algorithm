702e002cf6d6e80f378498628bb5feb791e541c7
Mun Juin <65991410+munjuin@users.noreply.github.com>
2026-03-13 11:59:29 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 5.02 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,4 +1,8 @@
 function solution(arr, queries) {
-    var answer = [];
-    return answer;
+    for(let [s, e, k] of queries) {
+        for(let i = s; i <= e; i++) {
+            if(i % k === 0) arr[i]++;
+        }
+    }
+    return arr;
 }
