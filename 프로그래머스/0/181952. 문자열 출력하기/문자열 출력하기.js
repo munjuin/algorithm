478687d30c9cc73d945560e25c8be2feb320357e
Mun Juin <65991410+munjuin@users.noreply.github.com>
2026-03-13 11:49:48 +0900

    [level 0] Title: 문자열 출력하기, Time: 61.64 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
@@ -1,20 +1,3 @@
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// rl.on('line', (line)=>{
-//     console.log(line);
-    
-//     rl.close();
-// })
-
-
-// rl.on('close', ()=>{
-//     process.exit();
-// })
-
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
 
