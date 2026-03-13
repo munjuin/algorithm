55a1d5a8d1f5d3526a251132254bf33be10cbc81
Mun Juin <65991410+munjuin@users.noreply.github.com>
2026-03-13 11:52:33 +0900

    [level 0] Title: a와 b 출력하기, Time: 43.71 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();// "4 5"
-const inputSplit = input.split(' ');// ["4", "5"]
-const [a, b] = inputSplit.map(Number);// [4, 5]
-console.log(`a = ${a}\nb = ${b}`)
-
+const split = input.split(' ');
+const [a, b] = split.map(Number);
+console.log(`a = ${a} \nb = ${b}`)
