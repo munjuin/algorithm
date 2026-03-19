7a0752241f03c4f9db92dfd057a407bd214848c5
Mun Juin <65991410+munjuin@users.noreply.github.com>
2026-03-19 10:41:10 +0900

    [level 0] Title: 양꼬치, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,7 @@
+function solution(n, k) {
+    if(n < 10){
+        return (n *12000) + (k * 2000);
+    }else if(n >= 10){
+        return (n * 12000) + (k * 2000) - (Math.floor(n / 10) * 2000);
+    }
+}
